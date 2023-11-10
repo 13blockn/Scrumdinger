868264b62dcc6e5fda330dd89b45ee22911a65b9
Nate Block <13blockn@gmail.com>
2023-11-09 21:05:34 -0600

    Begin repo

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nate Block on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
