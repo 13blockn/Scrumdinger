36faa05b8e14476d4b8742a3811c7499e64d9bec
Nate Block <13blockn@gmail.com>
2023-11-10 17:56:43 -0600

    Add detail view + navigation

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,53 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Nate Block on 11/10/23.
+//
+
+import SwiftUI
+
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .foregroundColor(.accentColor)
+                        .font(.headline)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }.navigationTitle(scrum.title)
+    }
+}
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
