7fc47793899ae7bfb16cc3dab79a3a69edfbbc99
Nate Block <13blockn@gmail.com>
2023-11-09 22:52:47 -0600

    Add meeting view + gitignore

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Nate Block on 11/9/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                     Image(systemName: "forward.fill")
+                 }
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
