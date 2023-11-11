c93278984ecda76f8f5a02a902ddb1f7b5d13671
Nate Block <13blockn@gmail.com>
2023-11-10 21:09:04 -0600

    Add names from the family

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -40,15 +40,15 @@ extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
         DailyScrum(title: "Design",
-                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   attendees: ["Aubrey", "Pepe", "Simon", "Jonathan"],
                    lengthInMinutes: 10,
                    theme: .yellow),
         DailyScrum(title: "App Dev",
-                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   attendees: ["Aubrey", "Gray", "Euna", "Luis", "Darla"],
                    lengthInMinutes: 5,
                    theme: .orange),
         DailyScrum(title: "Web Dev",
-                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   attendees: ["Aubrey", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
                    lengthInMinutes: 5,
                    theme: .poppy)
     ]
